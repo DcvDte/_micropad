c1e8af0a8e2f08ecf855e197ac8b54457c21ae06
rodrygombia <dcv.rodrigo.bonilla@gmail.com>
2016-11-09 15:45:12 -0300

    Agregado botones a el slider
    Se agregaron los botones al slider

--- assets2/plugins/layer-slider/layerslider/js/layer-slider.js
+++ assets2/plugins/layer-slider/layerslider/js/layer-slider.js
@@ -10,7 +10,7 @@ var LayerSlider = function () {
 			        responsive : true,
 			        responsiveUnder : 960,
 			        layersContainer : 960,
-			        skinsPath: 'assets/plugins/layer-slider/layerslider/skins/'
+			        skinsPath: 'assets2/plugins/layer-slider/layerslider/skins/'
 			    });
 		    });     
         }
